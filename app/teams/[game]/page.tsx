8314522110acb57a553048bfb084cfee50599b6a
ItsKensann <114538139+ItsKensann@users.noreply.github.com>
2026-04-18 23:22:39 -0700

    metadata for teams page

--- app/teams/[game]/page.tsx
+++ app/teams/[game]/page.tsx
@@ -1,11 +1,30 @@
-type Params = { game: string };
+import type { Metadata } from "next";
+// type Params = { game: string };
 
 type PageProps = {
   params: Promise<{ game: string }>;
 };
 
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const { game } = await params;
+
+  // fetch team data from db using this game id
+
+  return {
+    title: `EaOSU`,
+    description: `Check out the ${game} roster at EaOSU`,
+    openGraph: {
+      title: `${game} roster`,
+      description: ``,
+      images: [],
+    },
+  };
+}
+
 // fetches data(params) on the server
-export default async function TeamPage({ params }: { params: Params }) {
+export default async function TeamPage({ params }: PageProps) {
   const { game } = await params;
 
   return <div>{game + ` page`}</div>;
