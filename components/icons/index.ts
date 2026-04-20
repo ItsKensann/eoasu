a9c5a84f5c1a882282849498e8ed9e17f156ced7
ItsKensann <114538139+ItsKensann@users.noreply.github.com>
2026-04-19 19:33:37 -0700

    navbar complete
    - added social icons to the right
    - styled social icons
    - will need to add a dropdown to the teams nav item later once we implement the dynamic page logic for the teams
    - will now focus on the footer

---
+++ components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as TwitterIcon } from "./TwitterIcon";
+export { default as InstagramIcon } from "./InstagramIcon";
+export { default as TwitchIcon } from "./TwitchIcon";
+export { default as DiscordIcon } from "./DiscordIcon";
+export { default as YoutubeIcon } from "./YoutubeIcon";
--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -3,6 +3,13 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
+import {
+  InstagramIcon,
+  TwitterIcon,
+  YoutubeIcon,
+  TwitchIcon,
+  DiscordIcon,
+} from "../icons";
 import { cn } from "@/lib/utils";
 
 const navItems = [
@@ -11,20 +18,50 @@ const navItems = [
   { name: "About", path: "/about" },
 ];
 
+const socialLinks = [
+  {
+    name: "Instagram",
+    icon: InstagramIcon,
+    path: "https://www.instagram.com/osu.esports/",
+  },
+  {
+    name: "Twitter",
+    icon: TwitterIcon,
+    path: "https://x.com/OSUEsports",
+  },
+  {
+    name: "Youtube",
+    icon: YoutubeIcon,
+    path: "https://www.youtube.com/channel/UCFLeF0iSnJKIIdF6qKZrBgw",
+  },
+  {
+    name: "Twitch",
+    icon: TwitchIcon,
+    path: "https://www.twitch.tv/esportsatosu",
+  },
+  {
+    name: "Discord",
+    icon: DiscordIcon,
+    path: "https://discord.com/invite/KVt2JGxaaz",
+  },
+];
+
 export default function NavBar() {
   const pathname = usePathname();
 
   return (
-    <header className="fixed top-0 w-full z-50 bg-background/60 backdrop-blur-xl border-b">
+    <header className="fixed top-0 w-full z-50 bg-background/60 backdrop-blur-xl border-b ">
       <nav className="flex justify-between items-center px-6 md:px-12 h-20 w-full max-w-screen-2xl mx-auto">
         {/* EaOSU logo */}
         <div className="flex items-center gap-10">
           <div>
             <Image
               src={"/images/eaosu_logo.png"}
-              width={70}
-              height={80}
+              width={60}
+              height={70}
               alt="Logo of Esports at OSU"
+              style={{ width: "auto", height: "auto" }}
+              priority
             />
           </div>
 
@@ -35,7 +72,7 @@ export default function NavBar() {
                 key={item.name}
                 href={item.path}
                 className={cn(
-                  "transition-all duration-300 hover:text-white hover:drop-shadow-[0_0_8px_rgba(255,102,0,0.8)]",
+                  "transition-all font-sans duration-300 hover:text-white hover:drop-shadow-[0_0_8px_rgba(255,102,0,0.8)]",
                   pathname === item.path
                     ? "text-primary-container border-b-2 border-primary-container"
                     : "text-white/80",
@@ -48,7 +85,20 @@ export default function NavBar() {
         </div>
 
         {/* Social links */}
-        <div className="flex items-center">Social links</div>
+        <div className="flex items-center gap-4">
+          {socialLinks.map((item) => {
+            const Icon = item.icon;
+            return (
+              <Link key={item.name} href={item.path}>
+                <Icon
+                  className="fill-white hover:fill-primary-container transition-colors"
+                  width={25}
+                  height={25}
+                />
+              </Link>
+            );
+          })}
+        </div>
       </nav>
     </header>
   );
