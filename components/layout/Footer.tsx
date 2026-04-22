386f50cc45b1b20f5881698a4c643ea6e6990720
ItsKensann <114538139+ItsKensann@users.noreply.github.com>
2026-04-21 19:19:04 -0700

    footer skeleton code

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,3 +1,72 @@
+"use client";
+
+import Link from "next/link";
+import React, { useState } from "react";
+import Image from "next/image";
+import { Send } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+const socialLinks = [
+  {
+    name: "Instagram",
+    path: "https://www.instagram.com/osu.esports/",
+  },
+  {
+    name: "Twitter",
+    path: "https://x.com/OSUEsports",
+  },
+  {
+    name: "Youtube",
+    path: "https://www.youtube.com/channel/UCFLeF0iSnJKIIdF6qKZrBgw",
+  },
+  {
+    name: "Twitch",
+    path: "https://www.twitch.tv/esportsatosu",
+  },
+  {
+    name: "Discord",
+    path: "https://discord.com/invite/KVt2JGxaaz",
+  },
+];
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = () => {
+    // pass
+  };
+
+  return (
+    <footer className="bg-background border-t border-white/5 w-full py-12 md:px-12">
+      <div className="max-w-screen-2xl mx-auto flex flex-col md:flex-row justify-between items-center gap-8">
+        <div className="flex flex-col gap-4">
+          <div>
+            <Image
+              src={"/images/eaosu_logo.png"}
+              width={65}
+              height={70}
+              alt="Logo of Esports at OSU"
+              style={{ width: "auto", height: "auto" }}
+              priority
+            />
+          </div>
+          <p className="font-sans text-[11px] tracking-widest text-white/90 max-w-xs">
+            © Copyright 2026 - All rights reserved.
+          </p>
+        </div>
+        <div className="flex gap-4">
+          {socialLinks.map((item) => (
+            <Link
+              key={item.name}
+              href={item.path}
+              className="font-sans text-[12px] tracking-widest text-white/90 hover:text-primary-container transition-colors"
+            >
+              {item.name}
+            </Link>
+          ))}
+        </div>
+        <div>tesg</div>
+      </div>
+    </footer>
+  );
 }
