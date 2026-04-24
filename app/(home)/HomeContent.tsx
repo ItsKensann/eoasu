4f07f8424b9c0aaff70304f6fda5c2ec291c366f
ItsKensann <114538139+ItsKensann@users.noreply.github.com>
2026-04-23 22:43:20 -0700

    added images to hero section

--- app/(home)/HomeContent.tsx
+++ app/(home)/HomeContent.tsx
@@ -1,9 +1,46 @@
 "use client";
+// gonna put all the home page in same file for now then seperate them later once it looks passable
+import { motion } from "motion/react";
+import { useState, useEffect } from "react";
+import { ArrowRight, ArrowLeft, Verified } from "lucide-react";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+
+const images = [
+  { id: "image_1", src: "/images/hero_section/eaosu_slide1.gif" },
+  { id: "image_2", src: "/images/hero_section/eaosu_slide2.jpg" },
+  { id: "image_3", src: "/images/hero_section/eaosu_slide3.jpg" },
+  { id: "image_4", src: "/images/hero_section/eaosu_slide4.jpg" },
+  { id: "image_5", src: "/images/hero_section/eaosu_slide5.jpg" },
+];
 
 export default function HomeContent() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // side effect to change index of image every 5 seconds
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    // clean up timer
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <>
-      <div className="m">Hello world</div>
-    </>
+    <div className="flex flex-col w-full">
+      {/* Hero section */}
+      <section className="relative h-[65vh] min-h-[400px] w-full overflow-hidden flex items-end mt-20">
+        <div className="absolute inset-0 z-0 m-8">
+          <Image
+            src={images[currentIndex].src}
+            alt="Esports banner"
+            fill
+            priority
+            className="object-cover opacity-40"
+          />
+        </div>
+      </section>
+    </div>
   );
 }
